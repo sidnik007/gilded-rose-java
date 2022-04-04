511e5df59bb7d269601284876bb7b5b333c3eb59
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2022-04-04 20:04:58 +0530

    Sulfuras should never decrease in Quality.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -62,4 +62,12 @@ class GildedRoseTest {
         assertEquals(10, app.items[0].sellIn);
     }
 
+    @Test
+    void sulfarasShouldNeverDecreaseInQuality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
+
 }
