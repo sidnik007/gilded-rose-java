cd6b2203a573a913cde9923706994833e9190d16
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2022-04-05 21:14:41 +0530

    Inline for quick refactor.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -26,29 +26,33 @@ class GildedRose {
     private void handleQuality(Item item) {
         if (item.name.equals("Aged Brie")
             || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            } else {
+            inlineMeSoon(item);
+        } else {
             if (item.quality > 0) {
                 if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
                     return;
                 }
                 item.quality = item.quality - 1;
+            }
+        }
+    }
+
+    private void inlineMeSoon(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
             }
         }
     }
