27df1098501e3a1497366049d16369f69ba269da
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2022-04-06 13:04:02 +0530

    Refactor BackStagePassStrategy.

--- Java/src/main/java/com/gildedrose/BackStagePassStrategy.java
+++ Java/src/main/java/com/gildedrose/BackStagePassStrategy.java
@@ -4,25 +4,17 @@ public class BackStagePassStrategy extends ItemStrategy {
 
     @Override
     void handleQuality(Item item) {
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-                if (item.sellIn < 11) {
-                    incrementQualityIfBelowLimit(item);
-                }
-
-                if (item.sellIn < 6) {
-                    incrementQualityIfBelowLimit(item);
-                }
-            }
+        incrementQualityIfBelowLimit(item);
+        if (item.sellIn < 11) {
+            incrementQualityIfBelowLimit(item);
+        }
+        if (item.sellIn < 6) {
+            incrementQualityIfBelowLimit(item);
         }
     }
 
     @Override
     void handleExpired(Item item) {
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            item.quality = item.quality - item.quality;
-        }
+        item.quality = 0;
     }
 }
